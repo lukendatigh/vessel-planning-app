6f32e4ee5e1efcbdcf5cf002ce6670e7692c1983
jetNdatigh <lukendatigh@gmail.com>
2018-05-28 08:45:59 +0100

    class for data interaction and temp data objects

---
+++ ManningApp/Data.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using System.IO;
+
+namespace Data
+{  
+    class Database
+    {
+        public  SQLiteConnection con;
+        //database instance constructor and connection string declaration
+        public Database()
+        {
+            con = new SQLiteConnection("Data Source = manningAppDatabase.db; version=3");            
+        }
+
+        //opening connection
+        public void OpenConnection()
+        {
+            if (con.State != System.Data.ConnectionState.Open)
+            {
+                con.Open();
+            }
+            else return;
+        }
+
+        //closing connection
+        public void CloseConnection()
+        {
+            if (con.State != System.Data.ConnectionState.Closed)
+            {
+                con.Close();
+            }
+            else return;
+        }
+    }
+
+    class TempData
+    {
+        private string sessionName;
+
+        public void SetSessionUser(string name)
+        {
+            sessionName = name;
+        }
+    }
+
+
+}
