0a43d6ebffc8636de33943cab06c75d6d9932563
jetNdatigh <lukendatigh@gmail.com>
2018-06-07 10:40:53 +0100

    searching now works but not when enter key is pressed

---
+++ ManningApp/Dashboard_UserControls/Seafarer/SeafarerEdit.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using Data;
+
+namespace ManningApp.Dashboard_UserControls.Seafarer
+{
+    public partial class SeafarerEdit : UserControl
+    {
+        public SeafarerEdit()
+        {
+            InitializeComponent();
+        }
+
+        private void SeafarerEdit_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            searchSeafarer();
+        }
+        private void searchBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)13)
+            {
+                searchSeafarer();
+            }
+        }
+
+        private void btnUpdateSeafarer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchSeafarer()
+        {
+            string searchText = searchBox.Text;
+            Database database = new Database();
+            database.OpenConnection();
+
+            string query = "SELECT surname,othernames,rank,contract,previous_vessel,current_vessel " +
+                "FROM tblSeafarer WHERE surname LIKE '%" + searchText + "%' OR othernames LIKE '%" + searchText + "%'";
+            SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(query, database.connection);
+            using (dataAdapter)
+            {
+                DataTable dataTable = new DataTable("Seafarers");
+                dataAdapter.Fill(dataTable);
+                seafarerGridView.DataSource = dataTable;
+                DataView dataView = dataTable.DefaultView;
+                seafarerGridView.DataSource = dataView.ToTable();
+            }
+            database.CloseConnection();
+        }
+
+    }
+}
